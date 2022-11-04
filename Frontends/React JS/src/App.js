c9ad5f4e0f56329dba5953ba1ee9cc8842f6bf11
J004 <janith.kavinda3003@gmail.com>
2022-11-04 17:06:34 +0530

    successfully added the About page

--- Frontends/React JS/src/App.js
+++ Frontends/React JS/src/App.js
@@ -10,6 +10,7 @@ import Footer2 from './components/Footer2';
 import Footer3 from './components/Footer3';
 import Home from './components/Home';
 import NavBar from './components/Navbar';
+import About from './components/About'
 import './App.css'
 
 
@@ -18,11 +19,11 @@ class App extends Component
     render() {
         return(
           <div>
-            
             <BrowserRouter>
                 <div>
                   <Routes>
                   <Route path="/" exact element={<div className="App"><NavBar/><Home/><Footer3/></div>}/>
+                    <Route path='/about' element={<div><NavBar/><About/><Footer3/></div>}/>
                     <Route path="/login" element={<div><LoginPage/><Footer2/></div>}/>
                     <Route path="/flights" element={<div><Header/><Index/><Footer2/></div>}/>
                     <Route path="/add-flight" element={<div><Header/><AddFlight/><Footer2/></div>}/>
--- Frontends/React JS/src/components/About.js
+++ Frontends/React JS/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Home/About.css';
+import '../About.css';
 
 function About() {
   return (
