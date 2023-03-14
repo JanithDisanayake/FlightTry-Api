7930f7a53a6870aa047200e268d17f4e915c1cab
janithdisanayake <cohdse202f004@student.nibm.lk>
2023-03-14 15:49:42 +0530

    modified the Contact Compounent Interface

--- Frontends/React JS/src/components/Contact.js
+++ Frontends/React JS/src/components/Contact.js
@@ -1,29 +1,31 @@
 import React from "react";
 import "../Contact.css";
+import background from '../pexels-photo-2062462.jpeg'
 
 function Contact() {
   return (
-    <div className="contact">
+    <div className="contact ">
       <div
         className="leftSide"
-        style={{ backgroundImage: `url('https://i.picsum.photos/id/1018/3914/2935.jpg?hmac=3N43cQcvTE8NItexePvXvYBrAoGbRssNMpuvuWlwMKg')` }}
+        style={{ backgroundImage: `url(${background})` }}
       ></div>
       <div className="rightSide">
         <h1> Contact to the Director Board</h1>
 
         <form id="contact-form" method="POST">
-          <label htmlFor="name">Full Name</label>
-          <input name="name" placeholder="Enter full name..." type="text" />
-          <label htmlFor="email">Email</label>
-          <input name="email" placeholder="Enter email..." type="email" />
-          <label htmlFor="message">Message</label>
-          <textarea
-            rows="6"
-            placeholder="Enter message..."
-            name="message"
-            required
-          ></textarea>
-          <button type="submit"> Send Message</button>
+          <div className="mb-3">
+            <label className="form-label">Full Name</label>
+            <input name="name" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Email address</label>
+            <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+          <button className="btn btn-outline-secondary" type="submit"> Send Message</button>
         </form>
       </div>
     </div>
